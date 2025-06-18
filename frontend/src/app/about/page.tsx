b3558d7e4480f4f0f9a922b114240e96e4ec3121
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-06-19 00:15:28 +0530

    feat: Add About page for TradeConnect with introductory content

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            This is about page for TradeConnect, the premier B2B marketplace connecting verified buyers and sellers through intelligent RFQ management.
+        </div>
+    )
+}
+
+export default About
