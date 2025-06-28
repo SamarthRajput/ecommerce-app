a1723036e8717cf2f1c4e281bdfa324819bba70b
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-06-28 16:23:27 +0530

    tehgd

---
+++ backend/prisma/migrations/20250628105319_remove_temp_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tect` on the `RFQ` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RFQ" DROP COLUMN "tect";
