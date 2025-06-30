332e0d6e903859ee4dc9bb293e64c74462890642
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-06-30 13:07:52 +0530

    Refactor Cloudinary configuration to use constants from index file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,3 +5,15 @@ export const JWT_SECRET = process.env.JWT_SECRET || (() => {
 
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
 export const DEFAULT_ADMIN_ID = process.env.DEFAULT_ADMIN_ID || "4b62bc9f-6cc2-4e22-a932-9f4324b46d0d";
+
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || (() => {
+    throw new Error("CLOUDINARY_CLOUD_NAME not set in environment variables");
+})();
+
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || (() => {
+    throw new Error("CLOUDINARY_API_KEY not set in environment variables");
+})();
+
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || (() => {
+    throw new Error("CLOUDINARY_API_SECRET not set in environment variables");
+})();
